d00886e0de4eb7cfcdc29a9243bdbcec2185673f
jhoshuan-em <jelizaldem004@alumno.uaemex.mx>
2023-12-03 17:26:07 -0600

    Cambios en CRUD, ahora es un sistema DAO

--- GestionDulceria/src/ProductoMetodos.java
+++ GestionDulceria/src/ProductoMetodos.java
@@ -4,7 +4,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProductoMetodos implements CRUD {
+public class ProductoMetodos implements CRUD_DAO {
 	Connection con;
 	Conexion cn = new Conexion();
 	PreparedStatement ps;
